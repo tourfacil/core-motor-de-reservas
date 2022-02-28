43adc288bc049cb7c5fe6e3feb55af23d214638b
Fernando <fernando@elofy.com.br>
2022-02-28 13:53:52 -0300

    Adicionado metodo de geração de pedido pix

--- src/Services/PedidoService.php
+++ src/Services/PedidoService.php
@@ -1,4 +1,6 @@
-<?php namespace TourFacil\Core\Services;
+<?php
+
+namespace TourFacil\Core\Services;
 
 use Carbon\Carbon;
 use TourFacil\Core\Enum\AgendaEnum;
@@ -57,7 +59,7 @@ class PedidoService
             $servico = Servico::with([
                 'variacaoServico',
                 'fornecedor.splits',
-                'agendaServico.datasServico' => function($query) use ($servico_carrinho) {
+                'agendaServico.datasServico' => function ($query) use ($servico_carrinho) {
                     return $query->where('id', $servico_carrinho['agenda_selecionada']['data_servico_id']);
                 }
             ])->where('uuid', $servico_carrinho['uuid'])->first();
@@ -81,7 +83,7 @@ class PedidoService
             foreach ($servico_carrinho['agenda_selecionada']['variacoes'] as $variacao_carrinho) {
 
                 // Caso não tenha quantidade
-                if((!isset($variacao_carrinho['quantidade'])) || $variacao_carrinho['quantidade'] == 0) continue;
+                if ((!isset($variacao_carrinho['quantidade'])) || $variacao_carrinho['quantidade'] == 0) continue;
 
                 // Pega a variacao do serviço
                 $variacao_servico = $servico->variacaoServico->first(function ($item) use ($variacao_carrinho) {
@@ -92,7 +94,7 @@ class PedidoService
                 $valor_net_variacao = ($variacao_servico->percentual > 0) ? ($variacao_servico->percentual / 100) * $data_servico->valor_net : 0;
 
                 // Verifica se possui valores no NET para substituir
-                if(is_array($substitui_net)) {
+                if (is_array($substitui_net)) {
                     $valor_net_variacao = (string) number_format($valor_net_variacao, 2, ".", "");
                     $valor_net_variacao = ($substitui_net[$valor_net_variacao]) ?? $valor_net_variacao;
                 }
@@ -101,27 +103,27 @@ class PedidoService
                 $valor_venda_variacao = ($valor_net_variacao * $variacao_servico->markup);
 
                 // Verifica se o servico possui corretagem de valor
-                if($servico->tipo_corretagem != ServicoEnum::SEM_CORRETAGEM && ($valor_venda_variacao > 0)) {
+                if ($servico->tipo_corretagem != ServicoEnum::SEM_CORRETAGEM && ($valor_venda_variacao > 0)) {
 
                     // Verifica se a corretagem é em percentual
-                    if($servico->tipo_corretagem == ServicoEnum::CORRETAGEM_PORCENTUAL) {
+                    if ($servico->tipo_corretagem == ServicoEnum::CORRETAGEM_PORCENTUAL) {
                         $valor_venda_variacao += ($valor_venda_variacao / 100 * $servico->corretagem);
                     }
 
                     // Verifica se a corretagem é em valor fixo
-                    if($servico->tipo_corretagem == ServicoEnum::CORRETAGEM_FIXA) {
+                    if ($servico->tipo_corretagem == ServicoEnum::CORRETAGEM_FIXA) {
                         $valor_venda_variacao += $servico->corretagem;
                     }
                 }
 
                 // Verifica se possui valores da venda para substituir
-                if(is_array($substitui_venda)) {
+                if (is_array($substitui_venda)) {
                     $valor_venda_variacao = (string) number_format($valor_venda_variacao, 2, ".", "");
                     $valor_venda_variacao = $substitui_venda[$valor_venda_variacao] ?? $valor_venda_variacao;
                 }
 
                 /** Caso a variacao deva ser vendida por 1 real */
-                if($variacao_servico->percentual == 0 && $variacao_servico->markup == AgendaEnum::MARKUP_UM_REAL) {
+                if ($variacao_servico->percentual == 0 && $variacao_servico->markup == AgendaEnum::MARKUP_UM_REAL) {
                     $valor_venda_variacao = 1;
                 }
 
@@ -170,9 +172,9 @@ class PedidoService
             ];
 
             // Salva os dados para split de pagamento
-            if(isset(self::$pedido['split'][$servico->fornecedor->id])) {
+            if (isset(self::$pedido['split'][$servico->fornecedor->id])) {
                 self::$pedido['split'][$servico->fornecedor->id]['valor_net'] += (float) number_format($total_net_reserva, 2, ".", "");
-            } elseif(is_object($split_pagamento)) {
+            } elseif (is_object($split_pagamento)) {
                 self::$pedido['split'][$servico->fornecedor->id] = [
                     "token" => $split_pagamento->token,
                     "valor_net" => (float) number_format($total_net_reserva, 2, ".", "")
@@ -194,7 +196,7 @@ class PedidoService
     private static function comissaoTerminal($terminal)
     {
         // Caso nao seja venda no terminal
-        if(is_null($terminal)) return;
+        if (is_null($terminal)) return;
 
         // Percorre as reserva para calcular a comissao do terminal
         foreach (self::$pedido['reservas'] as $index => $reserva) {
@@ -231,7 +233,7 @@ class PedidoService
 
         // Monta o random number com o valores faltantes
         for ($i = 0; $i < $restante; $i++) {
-            $random_number .= mt_rand(0,9);
+            $random_number .= mt_rand(0, 9);
         }
 
         return $random_number;
@@ -270,7 +272,7 @@ class PedidoService
         ]);
 
         // Percorre cada servico para criar uma reserva
-        foreach($pedido_array["reservas"] as $reserva_carrinho) {
+        foreach ($pedido_array["reservas"] as $reserva_carrinho) {
 
             // Cria uma reserva para o servico selecionado
             $reserva = $pedido->reservas()->create([
@@ -313,19 +315,127 @@ class PedidoService
             ]);
 
             // Verifica se tem dados dos acompanhantes na reserva
-            if(isset($reserva_carrinho['acompanhantes']) && is_array($reserva_carrinho['acompanhantes'])) {
+            if (isset($reserva_carrinho['acompanhantes']) && is_array($reserva_carrinho['acompanhantes'])) {
                 // Salva os dados dos acompanhantes na reserva
                 $reserva->dadoClienteReservaPedido()->createMany($reserva_carrinho['acompanhantes']);
             }
 
             // Verifica se tem dados dados adicionais para a reserva
-            if(isset($reserva_carrinho['adicionais']) && is_array($reserva_carrinho['adicionais'])) {
+            if (isset($reserva_carrinho['adicionais']) && is_array($reserva_carrinho['adicionais'])) {
                 // Salva os dados adicionais na reserva
                 $reserva->campoAdicionalReservaPedido()->createMany($reserva_carrinho['adicionais']);
             }
 
             // Verifica se o pedido tem comissao de terminal
-            if(isset($reserva_carrinho['terminal'])) {
+            if (isset($reserva_carrinho['terminal'])) {
+
+                // Dados da comissao referente ao terminal
+                $dados_comissao = $reserva_carrinho['terminal'];
+
+                // Salva a comissao do terminal de venda
+                $pedido->comissaoTerminal()->create([
+                    'reserva_pedido_id' => $reserva->id,
+                    'terminal_id' => $dados_comissao['terminal_id'],
+                    'quantidade' => $dados_comissao['quantidade'],
+                    'comissao' => $dados_comissao['comissao'],
+                    'data_previsao' => $dados_comissao['previa_pagamento'],
+                    'status' => ComissaoStatus::AGUARDANDO,
+                ]);
+            }
+        }
+
+        return $pedido;
+    }
+
+    /**
+     * Gera um pedido para pix
+     *
+     * @param $pedido_array
+     * @param $payment
+     * @param $juros
+     * @param $cliente
+     * @param $canal_venda_id
+     * @param $origem
+     * @param $tipo_cartao
+     * @return mixed
+     */
+    public static function gerarPedidoPix($pedido_array, $cliente, $canal_venda_id, $origem, $metodo_pagamento)
+    {
+        // Cria o pedido
+        $pedido = Pedido::create([
+            "cliente_id" => $cliente->id,
+            "codigo" => $pedido_array['codigo_pedido'],
+            "valor_total" => $pedido_array['valor_total'],
+            "canal_venda_id" => $canal_venda_id,
+            "juros" => 0,
+            "origem" => $origem,
+            "status" => StatusPedidoEnum::PAGO,
+            "status_pagamento" => StatusPagamentoEnum::AUTORIZADO,
+            "metodo_pagamento" => $metodo_pagamento,
+        ]);
+
+        // Salva os dados da transacao
+        // $pedido->transacaoPedido()->create([
+        //     "transacao" => $payment
+        // ]);
+
+        // Percorre cada servico para criar uma reserva
+        foreach ($pedido_array["reservas"] as $reserva_carrinho) {
+
+            // Cria uma reserva para o servico selecionado
+            $reserva = $pedido->reservas()->create([
+                "servico_id" => $reserva_carrinho["servico_id"],
+                "fornecedor_id" => $reserva_carrinho["fornecedor_id"],
+                "agenda_data_servico_id" => $reserva_carrinho["agenda_data_servico_id"],
+                "valor_total" => $reserva_carrinho["valor_total"],
+                "valor_net" => $reserva_carrinho["valor_net"],
+                "quantidade" => $reserva_carrinho["quantidade"],
+                "bloqueio_consumido" => $reserva_carrinho["bloqueio_consumido"],
+                "status" => StatusReservaEnum::ATIVA
+            ]);
+
+            // Percorre as variacoes compradas
+            foreach ($reserva_carrinho["variacoes_reserva"] as $variacaoes_reserva) {
+
+                // Salva as quantidade adquiradas no pedido
+                $reserva->quantidadeReserva()->create([
+                    "variacao_servico_id" => $variacaoes_reserva["variacao_servico_id"],
+                    "quantidade" => $variacaoes_reserva["quantidade"],
+                    "valor_total" => $variacaoes_reserva["valor_total"],
+                    "valor_net" => $variacaoes_reserva["valor_net"],
+                ]);
+            }
+
+            // Diminui a quantidade da disponibilidade na agenda
+            $agenda_servico = AgendaDataServico::find($reserva_carrinho["agenda_data_servico_id"]);
+
+            // Quantidade disponivel final diminuido a quantidade do carrinho
+            $disponivel = $agenda_servico->disponivel - $reserva_carrinho["bloqueio_consumido"];
+
+            // Status da agenda
+            $status_agenda = ($disponivel >= 1) ? AgendaEnum::ATIVO : AgendaEnum::INDISPONIVEL;
+
+            // Atualiza a quantidade disponivel na agenda
+            $agenda_servico->update([
+                "disponivel" => $agenda_servico->disponivel - $reserva_carrinho["bloqueio_consumido"],
+                "consumido" => $agenda_servico->consumido + $reserva_carrinho["bloqueio_consumido"],
+                "status" => $status_agenda
+            ]);
+
+            // Verifica se tem dados dos acompanhantes na reserva
+            if (isset($reserva_carrinho['acompanhantes']) && is_array($reserva_carrinho['acompanhantes'])) {
+                // Salva os dados dos acompanhantes na reserva
+                $reserva->dadoClienteReservaPedido()->createMany($reserva_carrinho['acompanhantes']);
+            }
+
+            // Verifica se tem dados dados adicionais para a reserva
+            if (isset($reserva_carrinho['adicionais']) && is_array($reserva_carrinho['adicionais'])) {
+                // Salva os dados adicionais na reserva
+                $reserva->campoAdicionalReservaPedido()->createMany($reserva_carrinho['adicionais']);
+            }
+
+            // Verifica se o pedido tem comissao de terminal
+            if (isset($reserva_carrinho['terminal'])) {
 
                 // Dados da comissao referente ao terminal
                 $dados_comissao = $reserva_carrinho['terminal'];
