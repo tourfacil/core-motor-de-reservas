4f22e205a80992b31ecd2bd7a15c5af9d3ac1cfd
Vinicius Guimarães <viniciusbardina@gmail.com>
2022-04-02 10:44:28 -0300

    Desenvolvido método eficiente de busca de cupons válidos

--- src/Services/CupomDescontoService.php
+++ src/Services/CupomDescontoService.php
@@ -8,72 +8,52 @@ use TourFacil\Core\Models\CupomDesconto;
 abstract class CupomDescontoService
 {
     /**
-     * Retorna o cupom por código se utilizando das seguintes regras:
-     * Somente Cupons ativos, retorna o último cupom cadastrado que:
-     * Estiver ativo e estiver dentro do máximo de utilizações
+     * Retorna um cupom válido baseado nas seguintes regras:
+     * - Será o último cupom cadastrado com o $codigo informado
+     * - Será exibido se o status estiver como ´ATIVO´
+     * - Será exibido caso o número máximo de utilizações seja menor que o número de vezes utilizado
+     * - Ou Se o máximo_utilizações for igual a NULL, ou seja infinito
+     * - Caso seja um cupom para servico especifico, só será retornado caso o servico estiver de fato no carrinho
+     * - Será retornado somente um cupom e caso não tenha nenhum previsto com essas regras, será retornado NULL
      *
      * @param $codigo
      * @return mixed
      */
-    public static function getCupomDescontoByCodigo($codigo) {
+    public static function getCupomValidoByCodigo($codigo) {
 
+        // Busca o ID de todos os serviços que estão no carrinho
+        $ids_servicos_carrinho = self::getIDSProdutosCarrinho();
+
+        // Query que busca todos os cupons se usando das regras descritas na PHPDoc deste método
         $cupom = CupomDesconto::where('codigo', $codigo)
-                              ->whereNull('maximo_utilizacoes')
-                              ->orWhereColumn('vezes_utilizado', '<', 'maximo_utilizacoes')
-                              ->orderBy('id', 'desc')
-                              ->limit(1)
-                              ->where('status', '=', StatusDesconto::ATIVO)
-                              ->get()
-                              ->first();
-
-        return $cupom;
-    }
-
-    /**
-     * Retorna o cupom por código se utilizando das seguintes regras:
-     * Somente Cupons ativos, retorna o último cupom cadastrado que:
-     * Estiver ativo e estiver dentro do máximo de utilizações
-     * SOMENTE CUPONS QUE NÃO TENHAM UM SERVIÇO ESPECIFICO
-     *
-     * @param $codigo
-     * @return mixed
-     */
-    public static function getCupomDescontoByCodigoSemServicoEsp($codigo) {
-
-        $cupom = CupomDesconto::where('codigo', $codigo)
-            ->whereNull('maximo_utilizacoes')
-            ->orWhereColumn('vezes_utilizado', '<', 'maximo_utilizacoes')
-            ->orderBy('id', 'desc')
+            ->where('status', StatusDesconto::ATIVO)
+            ->where(function($query) use ($ids_servicos_carrinho) {
+                $query->whereNull('servico_id');
+                $query->orWhereIn('servico_id', $ids_servicos_carrinho);
+            })
+            ->where(function($query) {
+                $query->whereNull('maximo_utilizacoes');
+                $query->orWhereColumn('maximo_utilizacoes', '>', 'vezes_utilizado');
+            })
+            ->orderBy('id', 'Desc')
             ->limit(1)
-            ->where('status', '=', StatusDesconto::ATIVO)
-            ->whereNull('servico_id')
             ->get()
             ->first();
 
+        // Retorna o cupom
         return $cupom;
     }
 
-    /**
-     * Verifica se o serviço o qual o cupom da o desconto esta de fato no carrinho
-     *
-     * @param $codigo
-     * @return void
-     */
-    public static function isServicoDoCupomNoCarrinho($cupom) {
+    private static function getIDSProdutosCarrinho() {
 
-        // Pega os servicos do carrinho
         $servicos_carrinho = carrinho()->all();
+        $ids_servicos = [];
 
-        // Roda todos os serviços do carrinho
         foreach($servicos_carrinho as $servico_carrinho) {
-
-            // Caso ele encontre o servico do cupom no carrinho... Retorna true
-            if($servico_carrinho['gtin'] == $cupom->servico_id) {
-                return true;
-            }
+            $ids_servicos[] = $servicos_carrinho['gtin'];
         }
 
-        return false;
+        return $ids_servicos;
     }
 
     public static function aplicarCupomNaSessao($cupom) {
