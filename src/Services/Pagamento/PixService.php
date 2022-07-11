04bbcc04abb93c77a024fd5adae1f45acf4b1917
Vinicius Guimarães <viniciusbardina@gmail.com>
2022-07-11 09:32:49 -0300

    Adicionado função de criação de pedido por PIX

--- src/Services/Pagamento/PixService.php
+++ src/Services/Pagamento/PixService.php
@@ -2,12 +2,63 @@
 
 namespace TourFacil\Core\Services\Pagamento;
 
+use TourFacil\Core\Enum\MetodoPagamentoEnum;
+use TourFacil\Core\Models\Cliente;
+use TourFacil\Core\Services\Pagamento\Pagarme\Pix;
 
 /**
- * Class CartaoService
+ * Class PixService
  * @package TourFacil\Core\Services\Pagamento
  */
 class PixService
 {
-    
+
+    /**
+     * Realiza a cobranca no cartao de credito usando a Pagarme
+     *
+     * @param $array_pedido
+     * @param $cliente
+     * @return array
+     */
+    public static function gerarPixPagarme($array_pedido, $cliente)
+    {
+
+        $pix = new Pix();
+
+        $pix->setOrderCode($array_pedido['codigo_pedido']);
+
+        $pix->setCustomerName($cliente->nome);
+        $pix->setCustomerDocument($cliente->cpf);
+        $pix->setCustomerEmail($cliente->email);
+        $pix->setCustomerPhone($cliente->telefone);
+
+        $cliente = Cliente::where('id', $cliente->id)->with(['endereco'])->get()->first();
+
+        $pix->setExpiresIn(5);
+        $pix->setItems($array_pedido);
+
+        $codigo_pix = $pix->gerarCodigoPix();
+
+        // Caso seja aprovado
+        if ($codigo_pix['approved']) {
+            return [
+                "approved" => true,
+                "payment_id" => $codigo_pix['payment_id'],
+                "dados_pagamento" => [
+                    'gateway' => MetodoPagamentoEnum::PAGARME,
+                    'transacao' => $codigo_pix
+                ],
+            ];
+        }
+
+        // Caso haja algum tipo de erro
+        return [
+            "approved" => false,
+            "message" => $codigo_pix['erro'],
+            "dados_pagamento" => [
+                'gateway' => MetodoPagamentoEnum::PAGARME,
+                'transacao' => $codigo_pix
+            ],
+        ];
+    }
 }
