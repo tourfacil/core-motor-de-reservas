75af98e95ff1a8ddfa44fc636caa8b2fcdfa18c2
Vinicius Guimarães <viniciusbardina@gmail.com>
2022-12-17 10:15:25 -0300

    Adicionado cast de datas no Desconto e removidos dados antigos do fillable

--- src/Models/Desconto.php
+++ src/Models/Desconto.php
@@ -9,15 +9,21 @@ class Desconto extends Model
     protected $fillable = [
         'inicio',
         'final',
+        'inicio_utilizacao',
+        'final_utilizacao',
         'nome_publico',
         'nome_interno',
+        'desconto',
         'servico_id',
-        'total_vendido_venda',
-        'total_vendido_net',
-        'total_descontado_venda',
-        'total_descontado_net',
         'status',
         'tipo_desconto_valor',
         'tipo_desconto_fornecedor' ,
+    ];
+
+    protected $dates = [
+        'inicio',
+        'final',
+        'inicio_utilizacao',
+        'final_utilizacao'
     ];
 }
